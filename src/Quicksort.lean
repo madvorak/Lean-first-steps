c9723f44a04223d3cb35ad9a15a87dfd5c818cee
Martin Dvořák <martin.dvorak@matfyz.cz>
2021-08-28 12:14:06 +0200

    well-founded recursion in quicksort

--- src/Quicksort.lean
+++ src/Quicksort.lean
@@ -1,9 +1,30 @@
 import algebra.order
+import tactic
+
 
 
 private def only_those {T : Type} (cond : T → T → Prop) [∀ n, ∀ m, decidable (cond n m)] : T → list T → list T
 | p []        := []
 | p (x :: xs) := if (cond x p) then x :: only_those p xs else only_those p xs
+
+private lemma size_cap {T : Type} {cond : T → T → Prop} [∀ n, ∀ m, decidable (cond n m)] {pivot : T} {lizt : list T} :
+  (only_those cond pivot lizt).sizeof < (pivot :: lizt).sizeof
+  :=
+begin
+  induction lizt with head tail ih,
+    unfold only_those,
+    unfold list.sizeof,
+    linarith,
+
+    unfold list.sizeof,
+    have siz : (pivot :: tail).sizeof = (1 + (sizeof pivot) + tail.sizeof),
+      unfold list.sizeof,
+    -- by_cases cond head pivot,
+    calc (only_those cond pivot (head :: tail)).sizeof 
+         ≤ 1 + (sizeof head) + (only_those cond pivot tail).sizeof : by sorry     -- why cannot `unfold only_those` 
+    ...  < 1 + (sizeof head) + (pivot :: tail).sizeof              : by linarith  -- uses `ih` and `add_le_add` afaik
+    ...  = 1 + (sizeof head) + (1 + (sizeof pivot) + tail.sizeof)  : by linarith, -- uses `siz`
+end
 
 
 variable {L : Type}
@@ -18,12 +39,10 @@ private def only_gt : L → list L → list L :=
 
 def kviksort : list L → list L
 | []        := []
-| (x :: xs) := kviksort (only_le x xs) ++ [x] ++ kviksort (only_gt x xs)
-using_well_founded
-{ 
-  rel_tac := λ _ _, `[exact ⟨_, measure_wf (λ l, l.length)⟩],
-  dec_tac := `[ sorry ]
-}
+| (x :: xs) := have (only_le x xs).sizeof < (x :: xs).sizeof, from size_cap,
+               have (only_gt x xs).sizeof < (x :: xs).sizeof, from size_cap,
+               kviksort (only_le x xs) ++ [x] ++ kviksort (only_gt x xs)
 
 
-#eval kviksort [5, 8, 9, 9, 3, 2, 4, 20, 1, 6, 0, 7, 17, 5, 7, 11, 0, 4, 5, 6, 9, 15, 11, 18, 0, 5]
+#eval kviksort [14, 8, 2, 20, 15, 0, 11, 18, 7, 6, 3, 13, 10, 17, 1, 4, 5, 9, 19, 12, 16, 10]
+-- Result should be a sequence of integers 0..20 where 10 is twice.
